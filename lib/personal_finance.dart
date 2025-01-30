be352903a55d9c636bf8d48eb0d539fe6af28514
Elimbek Kasymbekov <elimbek.work49@gmail.com>
2025-01-31 02:14:11 +0600

    Added new widgets

---
+++ lib/personal_finance.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PersonalFinance extends StatefulWidget {
+  const PersonalFinance({super.key});
+
+  @override
+  State<PersonalFinance> createState() => _PersonalFinanceState();
+}
+
+class _PersonalFinanceState extends State<PersonalFinance> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
